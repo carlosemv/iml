faec627682f6b0f751783592ae656482c42c62d4
Carlos Vieira <vieiramecarlos@gmail.com>
2019-05-03 13:26:31 -0300

    Add exception handling for file reading

--- src/IO.cpp
+++ src/IO.cpp
@@ -1,10 +1,18 @@
 #include "IO.h"
+#include <iostream>
 
 namespace IO {
 
     std::string read_file(std::string filename)
     {
         std::ifstream in(filename, std::ios::in);
+        try {
+            in.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        } catch (const std::ios::failure& e) {
+            std::cerr << "Error reading " << filename << std::endl;
+            throw e;
+        }
+
         std::stringstream sstr;
         sstr << in.rdbuf();
         return sstr.str();
